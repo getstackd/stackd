257738e496a05aee94ca60f4055b8e1caa338af9
Justin Walsh <jwalsh@axonsports.com>
2014-07-21 03:12:05 -0700

    Xcode project stub

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  stacked
+//
+//  Created by Justin Walsh (Axon) on 7/20/14.
+//  Copyright (c) 2014 Justin Walsh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
+
